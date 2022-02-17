de1d20a77f18e4a06edc837927f1d6bfa88877c6
Nazar-kob <kobrinnazar05@mail.com>
2022-02-17 11:53:11 +0200

    new selution

--- app/bomb/bomb.py
+++ app/bomb/bomb.py
@@ -3,7 +3,15 @@ import pygame
 
 class Bomb(pygame.sprite.Sprite):
 
-    def __init__(self, position: tuple):
+    def __init__(self, position: tuple, default_obj_size: int):
         super(Bomb, self).__init__()
         self.surf = pygame.image.load("images/bomb.png").convert_alpha()
         self.rect = self.surf.get_rect(center=position)
+        self.default_obj_size = default_obj_size
+        self.rect.center = self.get_self_center()
+
+    def get_self_center(self):
+        return (
+            (self.rect.centerx // self.default_obj_size) * self.default_obj_size + self.rect.width // 2,
+            (self.rect.centery // self.default_obj_size) * self.default_obj_size + self.rect.height // 2,
+        )
--- app/main.py
+++ app/main.py
@@ -1,103 +1,15 @@
 import pygame
 
 from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
-    QUIT,
-    K_SPACE
+    QUIT
 )
 
 from app.bomb.bomb import Bomb
+from app.config import SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_OBJECT_SIZE
+from app.player.player import Player
 from app.wall.wall import Wall
-
-SCREEN_WIDTH = 650
-SCREEN_HEIGHT = 650
-DEFAULT_OBJECT_SIZE = 50
-
-
-class Player(pygame.sprite.Sprite):
-    DIFFERENCE_BETWEEN_OBJECT = 35
-
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.image.load("images/player_front.png").convert()
-        self.rect = self.surf.get_rect()
-        self.counter = 60
-        self.position_bomb = (0, 0)
-        self.get_bomb = None
-
-    def update(self, pressed_keys):
-        self.counter += 1
-
-        current_position = (self.rect.top,
-                            self.rect.left,
-                            self.rect.bottom,
-                            self.rect.right)
-
-        if pressed_keys[K_UP]:
-            self.surf = pygame.image.load("images/player_back.png").convert()
-            self.rect.move_ip(0, -5)
-            if pygame.sprite.spritecollideany(player, walls) or \
-                    pygame.sprite.spritecollideany(player, bombs):
-                self.rect.top = current_position[0]
-                self.rect.bottom = current_position[2]
-
-        if pressed_keys[K_DOWN]:
-            self.surf = pygame.image.load("images/player_front.png").convert()
-            self.rect.move_ip(0, 5)
-            if pygame.sprite.spritecollideany(player, walls) or \
-                    pygame.sprite.spritecollideany(player, bombs):
-                self.rect.top = current_position[0]
-                self.rect.bottom = current_position[2]
-
-        if pressed_keys[K_LEFT]:
-            self.surf = pygame.image.load("images/player_left.png").convert()
-            self.rect.move_ip(-5, 0)
-            if pygame.sprite.spritecollideany(player, walls) or \
-                    pygame.sprite.spritecollideany(player, bombs):
-                self.rect.left = current_position[1]
-                self.rect.right = current_position[3]
-
-        if pressed_keys[K_RIGHT]:
-            self.surf = pygame.image.load("images/player_right.png").convert()
-            self.rect.move_ip(5, 0)
-            if pygame.sprite.spritecollideany(player, walls) or \
-                    pygame.sprite.spritecollideany(player, bombs):
-                self.rect.left = current_position[1]
-                self.rect.right = current_position[3]
-
-        if pressed_keys[K_SPACE]:
-            if self.counter >= 60:
-                boom = Bomb(self.rect.center)
-                all_sprites.add(boom)
-                self.get_bomb = boom
-                self.position_bomb = boom.rect.center
-                self.counter = 0
-
-        position_player = self.rect.center
-        if (self.position_bomb[1] - Player.DIFFERENCE_BETWEEN_OBJECT > position_player[1] or
-            self.position_bomb[1] + Player.DIFFERENCE_BETWEEN_OBJECT < position_player[1]) or (
-                self.position_bomb[0] - Player.DIFFERENCE_BETWEEN_OBJECT > position_player[0] or
-                self.position_bomb[0] + Player.DIFFERENCE_BETWEEN_OBJECT < position_player[0]):
-
-            if self.get_bomb is not None:
-                bombs.add(self.get_bomb)
-                self.get_bomb = None
-                self.position_bomb = (0, 0)
-
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-
 
 if __name__ == '__main__':
     pygame.init()
@@ -132,7 +44,7 @@ if __name__ == '__main__':
 
         pressed_keys = pygame.key.get_pressed()
 
-        player.update(pressed_keys)
+        player.update(pressed_keys, walls, bombs, all_sprites, Bomb)
 
         walls.update()
 
