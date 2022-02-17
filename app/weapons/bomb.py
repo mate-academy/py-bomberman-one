3b5e353455fa94f7984a5003f44f04fba4df0c2b
Alex Zhidkov <sashazudkov.@gmail.com>
2022-02-17 08:23:03 +0200

    rename wall -> bomb

--- app/weapons/bomb.py
+++ app/weapons/bomb.py
@@ -13,15 +13,15 @@ class Bomb(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect(center=center_pos)
 
     @staticmethod
-    def create_centers_of_bombs(field_size: tuple, wall_size: tuple):
-        center_width = wall_size[0] // 2
-        center_height = wall_size[1] // 2
+    def create_centers_of_bombs(field_size: tuple, bomb_size: tuple):
+        center_width = bomb_size[0] // 2
+        center_height = bomb_size[1] // 2
         centers = []
 
         while center_height < field_size[1]:
             while center_width < field_size[0]:
                 centers.append((center_width, center_height))
-                center_width += 2 * wall_size[0] // 2
-            center_height += 2 * wall_size[1] // 2
-            center_width = wall_size[0] // 2
+                center_width += 2 * bomb_size[0] // 2
+            center_height += 2 * bomb_size[1] // 2
+            center_width = bomb_size[0] // 2
         return centers
