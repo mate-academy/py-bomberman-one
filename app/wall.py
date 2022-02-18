0ad2b0a4b60188fb98fa92c5a140d85cbe5e5210
Alex Gergel <alexgergel@gmail.com>
2022-02-18 13:30:44 +0200

    Solution

---
+++ app/wall.py
@@ -0,0 +1,29 @@
+import pygame
+
+SCREEN_WIDTH = 650
+SCREEN_HEIGHT = 650
+DEFAULT_OBJECT_SIZE = 50
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, center_pos: tuple):
+        super().__init__()
+        self.width = DEFAULT_OBJECT_SIZE
+        self.height = DEFAULT_OBJECT_SIZE
+        self.surf = pygame.image.load("images/wall.png")
+        self.rect = self.surf.get_rect(center=center_pos)
+
+    @staticmethod
+    def create_centers_of_walls(field_size: tuple, wall_size: tuple):
+        center_width = wall_size[0] + wall_size[0] // 2
+        center_height = wall_size[1] + wall_size[1] // 2
+        centers = []
+
+        while center_height < field_size[1] - wall_size[1]:
+            while center_width < field_size[0] - wall_size[0]:
+                centers.append((center_width, center_height))
+                center_width += 2 * wall_size[0]
+            center_height += 2 * wall_size[1]
+            center_width = wall_size[0] + wall_size[0] // 2
+
+        return centers
